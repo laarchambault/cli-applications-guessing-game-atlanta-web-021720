480b66367aae29687b1f28a95705dd7ae355339d
Laura Archambault <laarchambault@gmail.com>
2019-11-17 21:55:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,12 +3,25 @@ def generate_computer_answer
   c_a
 end
 
+def prompt_user
+  puts "Guess a number between 1 and 6."
+end
 
-
-
+def capture_guess
+  gets.chomp
+end
 
 
 
 def run_guessing_game
-  
+  computer_answer = generate_computer_answer
+  prompt_user
+  input = capture_guess
+  if input == 'exit'
+    puts "Goodbye!"
+    elsif input == computer_answer
+    puts "You guessed the correct number!"
+  else 
+    puts "Sorry! The computer guessed #{computer_answer}."
+  end
 end
